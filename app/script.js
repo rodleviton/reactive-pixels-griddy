e09b3436d3956f15d84c708ae7d9512b1a379dea
Rod Leviton <rod@rodleviton.com>
2017-12-17 15:05:10 +1100

    fix(grid): improve render loop perf

--- app/script.js
+++ app/script.js
@@ -1,4 +1,4 @@
-import './style.css';
+import "./style.css";
 
 const cellSize = 10;
 const canvasWidth = 340;
@@ -7,11 +7,11 @@ const canvasHeight = 460;
 const addLighting = (ctx, x, y, radius) => {
   ctx.save();
 
-  ctx.globalCompositeOperation = 'lighter';
+  ctx.globalCompositeOperation = "lighter";
   const radialGradient = ctx.createRadialGradient(x, y, 0, x, y, radius);
 
-  radialGradient.addColorStop(0.0, '#2A3178');
-  radialGradient.addColorStop(1, '#000000');
+  radialGradient.addColorStop(0.0, "#2A3178");
+  radialGradient.addColorStop(1, "#000000");
 
   ctx.fillStyle = radialGradient;
   ctx.beginPath();
@@ -24,15 +24,15 @@ const addLighting = (ctx, x, y, radius) => {
 const makeGrid = (canvasWidth, canvasHeight, cellWidth, cellHeight) => {
   const cellsX = Math.ceil(canvasWidth / cellWidth); // cells per row
   const cellsY = Math.ceil(canvasHeight / cellHeight); // rows
-  const fadeDirection = ['in', 'out']; 
+  const fadeDirection = ["in", "out"];
   const colours = [
-    '#407CEE',
-    '#386BE8',
-    '#315DC9',
-    '#354EB0',
-    '#39419C',
-    '#292F70',
-    '#242A62'
+    "#407CEE",
+    "#386BE8",
+    "#315DC9",
+    "#354EB0",
+    "#39419C",
+    "#292F70",
+    "#242A62"
   ];
 
   let grid = [];
@@ -42,14 +42,15 @@ const makeGrid = (canvasWidth, canvasHeight, cellWidth, cellHeight) => {
 
     for (let j = 0; j < cellsX; j++) {
       const cell = {
-        xPos: (j * cellWidth),
-        yPos: (i * cellHeight),
+        xPos: j * cellWidth,
+        yPos: i * cellHeight,
         width: cellWidth,
         height: cellHeight,
         speed: Math.random() * 0.02,
         opacity: Math.random(),
-        fadeDirection: fadeDirection[Math.floor(Math.random() * fadeDirection.length)],
-        background: colours[Math.floor(Math.random() * colours.length)]        
+        fadeDirection:
+          fadeDirection[Math.floor(Math.random() * fadeDirection.length)],
+        background: colours[Math.floor(Math.random() * colours.length)]
       };
 
       grid[i] = [...grid[i], cell];
@@ -60,57 +61,57 @@ const makeGrid = (canvasWidth, canvasHeight, cellWidth, cellHeight) => {
 };
 
 const render = (canvas, grid) => {
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext("2d");
 
   // Setup cell styles
   ctx.strokeStyle = "#0E151F";
   ctx.lineWidth = 0.5;
 
   const renderGrid = () => {
+    // Reset cell
+    ctx.fillStyle = "#292F70";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     grid.forEach((row, rowIndex) => {
       row.forEach((cell, cellIndex) => {
-        // Reset cell
-        ctx.fillStyle = '#292F70';
-        ctx.fillRect(cell.xPos, cell.yPos, cell.width, cell.height);
-        
         ctx.fillStyle = cell.background;
-                
-        if (cell.fadeDirection === 'in')  {
-          if ((cell.opacity + cell.speed) <= 1) {
+
+        if (cell.fadeDirection === "in") {
+          if (cell.opacity + cell.speed <= 1) {
             ctx.globalAlpha = cell.opacity + cell.speed;
-            cell.opacity = cell.opacity + cell.speed; 
-          } else {            
+            cell.opacity = cell.opacity + cell.speed;
+          } else {
             ctx.globalAlpha = cell.opacity - cell.speed;
             cell.opacity = cell.opacity - cell.speed;
-            cell.fadeDirection = 'out';
+            cell.fadeDirection = "out";
           }
         } else {
-          if ((cell.opacity - cell.speed) >= 0) {
+          if (cell.opacity - cell.speed >= 0) {
             ctx.globalAlpha = cell.opacity - cell.speed;
             cell.opacity = cell.opacity - cell.speed;
           } else {
             ctx.globalAlpha = cell.opacity + cell.speed;
             cell.opacity = cell.opacity + cell.speed;
-            cell.fadeDirection = 'in';
+            cell.fadeDirection = "in";
           }
         }
 
         ctx.fillRect(cell.xPos, cell.yPos, cell.width, cell.height);
         ctx.strokeRect(cell.xPos, cell.yPos, cell.width, cell.height);
-        
+
         ctx.globalAlpha = 1;
       });
     });
 
     addLighting(ctx, canvasWidth / 2, canvasHeight / 2, 200);
     requestAnimationFrame(renderGrid);
-  }
+  };
 
   return renderGrid;
-}
+};
 
 const renderGrid = render(
-  document.getElementById('pixels'), 
+  document.getElementById("pixels"),
   makeGrid(canvasWidth, canvasHeight, cellSize, cellSize)
 );
 
